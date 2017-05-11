b48cfcef7e578d001ec7f487059c21031d6b1586
Benson <benson@bensondeMacBook-Pro.local>
2017-05-11 13:43:47 +0800

    首页完成

--- app/containers/Home/subpage/List.js
+++ app/containers/Home/subpage/List.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
-import HomeAd from '../../../components/HomeAd';
+import ListComponent from '../../../components/List';
+import LoadMore from '../../../components/LoadMore';
 import { getListData } from '../../../fetch/home/home';
+
+import './style.less';
 
 class List extends React.Component{
     constructor(props, context){
         super(props, context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
         this.state = {
-            data: [],
-            hasMore: false
+            data: [],   // 存储列表信息
+            hasMore: false, // 记录当前状态下，还有没有更多的数据可供加载
+            isLoadingMore: false, // 记录当前状态下，是“加载中...”还是“点击加载更多”
+            page: 1, // 下一页的页码
         };
     }
     render(){
         return(
             <div>
                 <h2 className="home-list-title">猜你喜欢</h2>
-                {this.state.hasMore.toString()}
-                {this.state.data.length}
+                {
+                    this.state.data.length
+                    ? <ListComponent data={this.state.data}/>
+                    : <div>加载中...</div>
+                }
+                {
+                    this.state.hasMore
+                    ? <LoadMore isLoadingMore={this.state.isLoadingMore} loadMoreFn={this.loadMoreData.bind(this)} />
+                    : <div></div>
+                }
             </div>
         );
     }
@@ -32,6 +45,7 @@ class List extends React.Component{
     }
     // 数据处理
     resultHandle(result){
+        // 解析数据，更改state
         result.then((res) => {
             return res.json();
         }).then((json) => {
@@ -40,9 +54,26 @@ class List extends React.Component{
 
             // 存储数据
             this.setState({
-                data: data,
-                hasMore: hasMore
+                hasMore: hasMore,
+                data: this.state.data.concat(data)  // 拼接数据
             });
+        });
+    }
+    // 加载更多数据
+    loadMoreData(){
+        // 记录状态
+        this.setState({
+            isLoadingMore: true
+        });
+        const cityName = this.props.cityName;
+        const page = this.state.page; // 下一页页码
+        const result = getListData(cityName, page);
+        this.resultHandle(result);
+
+        // 添加 page 计数
+        this.setState({
+            page: page + 1,
+            isLoadingMore: false
         });
     }
 }
